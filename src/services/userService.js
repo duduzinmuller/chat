188a66aa7f0701dda0012e8c9c893d376b128712
Eduardo <eduardomuller989@gmail.com>
2024-12-27 07:23:58 -0400

    feat: add createUser

---
+++ src/services/userService.js
@@ -0,0 +1,27 @@
+// services/userService.js
+import prisma from "../utils/prismaClient";
+
+export const createUser = async (name, phone) => {
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name,
+                phone,
+            },
+        });
+        return user;
+    } catch (error) {
+        throw new Error("Error creating user", error);
+    }
+};
+
+export const getUserByPhone = async (phone) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { phone },
+        });
+        return user;
+    } catch (error) {
+        throw new Error("Error fetching user", error);
+    }
+};
