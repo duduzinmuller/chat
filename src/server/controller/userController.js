9b21c941656cf87cb5516c9fb12be918d60290c7
Eduardo <eduardomuller989@gmail.com>
2024-12-27 07:29:10 -0400

    feat: add createUserController and getUserController

---
+++ src/server/controller/userController.js
@@ -0,0 +1,27 @@
+// controllers/userController.js
+import { createUser, getUserByPhone } from "../services/userService";
+
+export const createUserController = async (req, res) => {
+    const { name, phone } = req.body;
+
+    try {
+        const user = await createUser(name, phone);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const getUserController = async (req, res) => {
+    const { phone } = req.params;
+
+    try {
+        const user = await getUserByPhone(phone);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
