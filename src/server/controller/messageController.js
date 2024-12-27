65b1450ad5f526d0516497e99051c1b5828285bb
Eduardo <eduardomuller989@gmail.com>
2024-12-27 07:25:17 -0400

    feat: add createMessageController and getMessagesController

---
+++ src/server/controller/messageController.js
@@ -0,0 +1,23 @@
+import { createMessage, getMessagesByUserId } from "../services/messageService";
+
+export const createMessageController = async (req, res) => {
+    const { userId, content, fileUrl, fileType } = req.body;
+
+    try {
+        const message = await createMessage(userId, content, fileUrl, fileType);
+        res.status(201).json(message);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const getMessagesController = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const messages = await getMessagesByUserId(userId);
+        res.status(200).json(messages);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
