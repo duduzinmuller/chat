af4b1774efd843a1f20c999047d2681a79f4128b
Eduardo <eduardomuller989@gmail.com>
2024-12-27 07:21:08 -0400

    feat: add createMessage

---
+++ src/server/controller/messageService.js
@@ -0,0 +1,31 @@
+import prisma from "../../utils/prismaClient";
+
+export const createMessage = async (userId, content, fileUrl, fileType) => {
+    try {
+        const message = await prisma.message.create({
+            data: {
+                content,
+                fileUrl,
+                fileType,
+                userId,
+            },
+        });
+        return message;
+    } catch (error) {
+        throw new Error("Error creating message");
+    }
+};
+
+export const getMessagesByUserId = async (userId) => {
+    try {
+        const messages = await prisma.message.findMany({
+            where: { userId },
+            orderBy: {
+                createdAt: "asc",
+            },
+        });
+        return messages;
+    } catch (error) {
+        throw new Error("Error fetching messages");
+    }
+};
