a9e7336b5b0b8115994fd76beb1ad13893225440
Eduardo <eduardomuller989@gmail.com>
2024-12-27 20:36:11 -0400

    feat: add sendVerificationCodeController

---
+++ src/server/controller/emailVerificationController.js
@@ -0,0 +1,42 @@
+import {
+    createEmailVerification,
+    verifyEmailCode,
+} from "../../services/emailVerificationService.js";
+
+export const sendVerificationCodeController = async (req, res) => {
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ message: "Email e necessario" });
+    }
+
+    try {
+        const verification = await createEmailVerification(email);
+        res.status(200).json({
+            message: "Código de verificação enviado com sucesso!",
+            verification,
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const verifyCodeController = async (req, res) => {
+    const { email, code } = req.body;
+
+    if (!email || !code) {
+        return res
+            .status(400)
+            .json({ message: "Email e codigo são necessarios." });
+    }
+
+    try {
+        const verification = await verifyEmailCode(email, code);
+        res.status(200).json({
+            message: "Codigo de verificação valido!",
+            verification,
+        });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
