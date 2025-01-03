ffd6d8b51c3fb45d76dc049b0e3fce7a7402c1cc
Eduardo <eduardomuller989@gmail.com>
2025-01-03 11:19:33 -0400

    feat: add findOrAuthenticateUser

--- src/services/authService.js
+++ src/services/authService.js
@@ -28,3 +28,61 @@ export const loginUser = async (email, password) => {
         contact: { id: contact.id, email: contact.email, name: contact.name },
     };
 };
+
+export const findOrAuthenticateUser = async (
+    name,
+    email,
+    authProvider,
+    providerId,
+    imageUrl = null,
+    bio = null,
+    phone,
+) => {
+    try {
+        const defaultImage = process.env.PHOTO;
+
+        const whereCondition =
+            authProvider === "GOOGLE"
+                ? { googleId: providerId }
+                : authProvider === "FACEBOOK"
+                  ? { facebookId: providerId }
+                  : null;
+
+        if (!whereCondition) {
+            throw new Error("Invalid auth provider");
+        }
+
+        const user = await prisma.contact.findUnique({
+            where: whereCondition,
+        });
+
+        if (user) {
+            return await prisma.contact.update({
+                where: { id: user.id },
+                data: {
+                    name,
+                    email,
+                    bio,
+                    imageUrl: imageUrl || defaultImage,
+                    phone,
+                },
+            });
+        } else {
+            return await prisma.contact.create({
+                data: {
+                    name,
+                    email,
+                    authProvider,
+                    googleId: authProvider === "GOOGLE" ? providerId : null,
+                    facebookId: authProvider === "FACEBOOK" ? providerId : null,
+                    imageUrl: imageUrl || defaultImage,
+                    bio,
+                    phone,
+                },
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        throw new Error("Error handling social login");
+    }
+};
