b3bb11d9a0f1aa6b40bedd90ffe6e57b4e54a254
Eduardo <eduardomuller989@gmail.com>
2025-01-07 16:35:32 -0400

    fix: delete https

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ import authRoutes from "./src/routes/authRoutes.js";
 import resetPasswordRouter from "./src/routes/resetPasswordRouter.js";
 
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+);
 app.use(express.json());
 
 // Routes
