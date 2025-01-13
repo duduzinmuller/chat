b1a7e7bf71ac467fbb1d165fdbb548111dbf5845
Eduardo <eduardomuller989@gmail.com>
2025-01-13 09:27:49 -0400

    feat: add getContact

---
+++ src/server/controller/getByContactController.js
@@ -0,0 +1,20 @@
+import { getContactById } from "../../services/getByContactService.js";
+
+export const getContact = async (req, res) => {
+    try {
+        const contact = await getContactById(req.params.id);
+        res.status(200).json(contact);
+    } catch (error) {
+        if (error.message === "Contato não encontrado") {
+            res.status(404).json({ message: error.message });
+        } else if (
+            error.message ===
+            "ID inválido. Deve ser um número inteiro positivo."
+        ) {
+            res.status(400).json({ message: error.message });
+        } else {
+            console.error(error);
+            res.status(500).json({ message: "Erro interno no servidor" });
+        }
+    }
+};
