4251d6613eb7caf5248c30a62d9c8edc98b22b93
Eduardo <eduardomuller989@gmail.com>
2025-01-14 09:29:17 -0400

    feat: add FriendListService

---
+++ src/services/listFriendsService.js
@@ -0,0 +1,29 @@
+import prisma from "../utils/prismaClient.js";
+
+export class FriendListService {
+    static async listFriends(contactId) {
+        try {
+            const contactWithFriends = await prisma.contact.findUnique({
+                where: { id: contactId },
+                select: {
+                    friends: {
+                        select: {
+                            id: true,
+                            name: true,
+                            email: true,
+                            imageUrl: true,
+                        },
+                    },
+                },
+            });
+
+            if (!contactWithFriends) {
+                throw new Error("Contato não encontrado");
+            }
+
+            return contactWithFriends.friends;
+        } catch (error) {
+            throw new Error(error.message || "Erro ao buscar lista de amigos");
+        }
+    }
+}
